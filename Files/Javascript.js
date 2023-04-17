15c3179a7c9c85b912c865dcd506dbbbc729a519
BenC <bcuttance1998@gmail.com>
2023-04-17 13:58:43 +0930

    started adding pseudo code for other developers to understand code. Changed location of where ReadME is located

--- Files/Javascript.js
+++ Files/Javascript.js
@@ -1,12 +1,14 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//Variables to call upon throughout the code
+
 var timeDisplay = document.querySelector("#currentDay")
 var timeBlock = document.querySelector('.time-block')
 var saveBtn = document.querySelectorAll('.saveBtn');
 var inputTextArea = document.querySelector('.description')
 var hour12 = document.querySelector('#hour-12')
 var allButtons = document.querySelectorAll('p')
+
+
+
 
 $(document).ready(function() {
     $(".saveBtn").on('click', function() {
@@ -18,41 +20,18 @@ $(document).ready(function() {
 })
 
 
-// $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
 
-
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-
- 
-
-
-
-
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-
+// calls the currentTime function every 1000 milliseconds (1 second) to update timer in heading
     setInterval(currentTime, 1000);
 
+
+// function that calls ti
     function currentTime() {
         $(timeDisplay).text(dayjs().format('dddd, MMMM D, h:mm:ss a'));
     }
 
 
-// });
+
 
 
 var hourly = dayjs();
